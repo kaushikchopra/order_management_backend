3992048a15f75aceaaede8ed2cc8b172fd20a9b7
S Kaushik Chopra <kaushikchopra2908@gmail.com>
2023-11-06 01:10:33 +0530

    Modify CORS options

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,7 +4,7 @@ const corsOptions = {
     origin: (origin, callback) => {
         // console.log(origin)
         // !origin is set for development phase only (i.e) it is to make cors available for localhost when an origin is undefined
-        if (allowedOrigins.indexOf(origin) !== -1) { // since it is an open source project !origin is set
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // since it is an open source project !origin is set
             callback(null, true)
         } else {
             // console.log(`Origin not allowed: ${origin}`);
